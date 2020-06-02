f819c085317e16d7e25f5a5fb8425152b7e65d31
Zh <64052158+windcrazy2000@users.noreply.github.com>
2020-06-02 16:54:00 +0800

    完成游戏库界面构造

--- app/src/main/java/cn/edu/scujcc/youxizixun/youxiku.java
+++ app/src/main/java/cn/edu/scujcc/youxizixun/youxiku.java
@@ -3,19 +3,18 @@ package cn.edu.scujcc.youxizixun;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Gallery;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class youxiku extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_youxiku);
     }
+
+
 
 }
