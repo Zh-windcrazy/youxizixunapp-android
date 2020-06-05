b7d5ca06cb863af178b521026cf246ee5845cc25
Zh <64052158+windcrazy2000@users.noreply.github.com>
2020-06-05 08:29:22 +0800

    完成界面跳转，修改界面内容

--- app/src/main/java/cn/edu/scujcc/youxizixun/BangdanActivity.java
+++ app/src/main/java/cn/edu/scujcc/youxizixun/BangdanActivity.java
@@ -2,7 +2,9 @@ package cn.edu.scujcc.youxizixun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class BangdanActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class BangdanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bangdan);
+        Button youxikuButton = findViewById(R.id.youxikubutton);
+        youxikuButton.setOnClickListener(v ->{
+            Intent intent = new Intent(BangdanActivity.this,youxiku.class);
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/cn/edu/scujcc/youxizixun/youxiku.java
+++ app/src/main/java/cn/edu/scujcc/youxizixun/youxiku.java
@@ -2,6 +2,7 @@ package cn.edu.scujcc.youxizixun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Gallery;
@@ -14,8 +15,13 @@ public class youxiku extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_youxiku);
-
+        Button bangdanButton = findViewById(R.id.bangdanbutton);
+        bangdanButton.setOnClickListener(v ->{
+            Intent intent = new Intent(youxiku.this,BangdanActivity.class);
+            startActivity(intent);
+        });
     }
+
 
 
 }
