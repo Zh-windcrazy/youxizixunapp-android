41069c60877314afeb16f53dddc8f9f481f40243
Zh <64052158+windcrazy2000@users.noreply.github.com>
2020-06-10 20:18:01 +0800

    完成登录界面

--- app/src/main/java/cn/edu/scujcc/youxizixun/LoginActivity.java
+++ app/src/main/java/cn/edu/scujcc/youxizixun/LoginActivity.java
@@ -1,20 +1,88 @@
 package cn.edu.scujcc.youxizixun;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button loginButton;
+    private UserLab lab = UserLab.getInstance();
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            if (null != msg) {
+                switch (msg.what) {
+                    case UserLab.USER_LOGIN_SUCCESS:
+                        loginSucess();
+                        break;
+                    case UserLab.USER_LOGIN_PASSWORD_ERROR:
+                        loginPasswordError();
+                        break;
+                    case UserLab.USER_LOGIN_NET_ERROR:
+                        loginFailed();
+                        break;
+                }
+            }
+        }
+    };
+
+    private void loginPasswordError() {
+        Toast.makeText(LoginActivity.this,
+                "密码错误，请重试！",
+                Toast.LENGTH_LONG).show();
+    }
+
+    private void loginSucess() {
+        Toast.makeText(LoginActivity.this,
+                "登录成功！",
+                Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(LoginActivity.this, youxikuActivity.class);
+        startActivity(intent);
+    }
+
+    private void loginFailed() {
+        Toast.makeText(LoginActivity.this,
+                "服务器错误，请稍候再试！",
+                Toast.LENGTH_LONG).show();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(v -> {
+            EditText username = findViewById(R.id.r_username);
+            EditText password = findViewById(R.id.login_password);
+            String u = username.getText().toString();
+            String p = password.getText().toString();
+            //TODO 调用Retrofit
+            lab.login(u, p, handler);
+        });
+
         Button registerButton = findViewById(R.id.register_button);
         registerButton.setOnClickListener(v ->{
+            Intent intent = new Intent(LoginActivity.this,youxikuActivity.class);
+            startActivity(intent);
+        });
+
+        Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(v ->{
+            Toast.makeText(LoginActivity.this,
+                    "登录成功！",
+                    Toast.LENGTH_LONG).show();
             Intent intent = new Intent(LoginActivity.this,youxikuActivity.class);
             startActivity(intent);
         });
---
+++ app/src/main/java/cn/edu/scujcc/youxizixun/User.java
@@ -0,0 +1,93 @@
+package cn.edu.scujcc.youxizixun;
+
+import java.util.Date;
+
+public class User {
+    private String id;
+    private String username;
+    private String password;
+    private String phone;
+    private String gender;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", phone='" + phone + '\'' +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (gender == null) {
+            if (other.gender != null)
+                return false;
+        } else if (!gender.equals(other.gender))
+            return false;
+        if (password == null) {
+            if (other.password != null)
+                return false;
+        } else if (!password.equals(other.password))
+            return false;
+        if (phone == null) {
+            if (other.phone != null)
+                return false;
+        } else if (!phone.equals(other.phone))
+            return false;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
+        return true;
+    }
+}
